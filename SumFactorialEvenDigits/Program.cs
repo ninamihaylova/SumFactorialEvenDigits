f437bd82630bf86ccb6e9dbacb5359304ee139c9
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-03 19:01:38 +0300

    Initial commit

---
+++ SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program {
+    static void Main() {
+        int n = int.Parse(Console.ReadLine());
+        int result = SumFactorialEvenDigits(n);
+        Console.WriteLine(result);
+    }
+
+    static int Factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    static int SumFactorialEvenDigits(int number) {
+        string digits = number.ToString();
+        int sum = 0;
+        
+        foreach (char digitChar in digits) {
+            int digit = int.Parse(digitChar.ToString());
+            
+            if (digit % 2 == 0) {
+                sum += Factorial(digit);
+            }
+        }
+        
+        return sum;
+    }
+}
+
+
